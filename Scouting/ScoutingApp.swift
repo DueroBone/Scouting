6c5136a1c039bd9c206e1a57dcb2f86718f4870f
Duel M <dueromoods@gmail.com>
2023-06-20 15:50:00 -0400

    Initial Commit

---
+++ Scouting/ScoutingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutingApp.swift
+//  Scouting
+//
+//  Created by Duel Mood on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ScoutingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
